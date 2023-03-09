44b9bd17773087089730435c82c3a6aa792429da
simolorevg <simoneviaggi@libero.it>
2023-03-09 13:29:21 +0100

    starting the script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert('Hello world');
+const submitBtn = document.getElementById("submit-btn");
+let finalPrice;
+submitBtn.addEventListener("click", function() {
+    const userNameInput = document.getElementById('username');
+    const userName = userNameInput.value;
+    console.log(userName);
+    const kmChoiceInput = document.getElementById('km-choice');
+    const kmChoice = parseInt(kmChoiceInput.value);
+    console.log(kmChoice, typeof kmChoice);
+    const userAgeInput = document.getElementById('select-age');
+    const userAge = userAgeInput.value;
+    console.log (userAge);
+});

b93a0a3cef4eec23f7da8c57cbd29d7803198306
simolorevg <simoneviaggi@libero.it>
2023-03-09 17:00:50 +0100

    Working to the style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const submitBtn = document.getElementById("submit-btn");
 let finalPrice;
 let message;
+let idNumber= Math.floor(Math.random() * (200000 - 1000) ) + 1000;
+let carriage = Math.floor( (Math.random() * 10) + 1);
 submitBtn.addEventListener("click", function() {
     //memorzzazione dati e conversione in value
     const userNameInput = document.getElementById('username');
@@ -14,22 +16,26 @@ submitBtn.addEventListener("click", function() {
         const userAge = userAgeInput.value;
         console.log (userAge);
         finalPrice = kmChoice * 0.21;
-        message="Prezzo pieno:";
+        message="nessuna";
         //calcolo prezzo e eventuale sconto
         if( userAge === "minorenne"){
             finalPrice = finalPrice - (finalPrice * 0.2);
-            message = "Sconto Under 18:";
+            message = "Sconto Under 18";
         }else if ( userAge === "Over65"){
             finalPrice = finalPrice - (finalPrice *0.4);
-            message = "Sconto Over 65:";
+            message = "Sconto Over 65";
         }
         finalPrice = finalPrice.toFixed(2);
-        message =message + " " + finalPrice + "€";
+        finalPrice = finalPrice + "€";
         userNameInput.value = "";
         kmChoiceInput.value = "";
         userAgeInput.value = "";
     }else{
-        message = "DATI NON INSERITI CORRETTAMENTE. RICARICARE LA PAGINA";
+        alert('ERRORE: DATI NON INSERITI CORRETTAMENTE. RICARICARE LA PAGINA.')
     }
-    document.getElementById('message').innerHTML = message;
+    document.getElementById('utente').innerHTML = userName;
+    document.getElementById('price').innerHTML = finalPrice;
+    document.getElementById('plain').innerHTML = message;
+    document.getElementById('carriage').innerHTML = carriage;
+    document.getElementById('idNumber').innerHTML = idNumber;
 });
